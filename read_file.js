ce743b2ffbee795a1fa02d14cec5dc5e7687c2fc
rortega <ortega.r.rafael@gmail.com>
2025-05-09 12:18:01 -0500

    Update read_file.js

--- read_file.js
+++ read_file.js
@@ -2,9 +2,9 @@
              onEnter: function(args) {
                 var filename = Memory.readCString(ptr(args[0]));
                 var x = Memory.readCString(ptr(args[1]));
-                console.log('filename =', filename)
-                console.log('filename =', x)
+                console.log('filename =', filename);
+                console.log('filename =', x);
               },
               onLeave: function(retval) {
               }
-            });
+});
