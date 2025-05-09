fffc9e7016fef0deeb599ce190723106d6e5258d
rortega <ortega.r.rafael@gmail.com>
2025-05-09 12:14:27 -0500

    Create read_file.js

---
+++ read_file.js
@@ -0,0 +1,10 @@
+ Interceptor.attach(Module.findExportByName("/system/lib/libc.so", "open"), {
+             onEnter: function(args) {
+                var filename = Memory.readCString(ptr(args[0]));
+                var x = Memory.readCString(ptr(args[1]));
+                console.log('filename =', filename)
+                console.log('filename =', x)
+              },
+              onLeave: function(retval) {
+              }
+            });
